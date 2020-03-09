f8500d745313702261edeae001ab5fea23a32555
bjornbrodtkorb <bjorn.thoralf@outlook.com>
2020-03-09 07:30:47 -0700

    Programmer fully working

--- BlackOS Programmer/main.Designer.cs
+++ BlackOS Programmer/main.Designer.cs
@@ -29,28 +29,21 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.loading_bar = new System.Windows.Forms.Panel();
             this.program_button = new System.Windows.Forms.Button();
             this.browse_button = new System.Windows.Forms.Button();
             this.file_path_text_box = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.com_port_box = new System.Windows.Forms.ComboBox();
             this.serial_port = new System.IO.Ports.SerialPort(this.components);
+            this.loading_bar = new System.Windows.Forms.Panel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.loading_label = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // loading_bar
-            // 
-            this.loading_bar.BackColor = System.Drawing.Color.SpringGreen;
-            this.loading_bar.Location = new System.Drawing.Point(12, 109);
-            this.loading_bar.Name = "loading_bar";
-            this.loading_bar.Size = new System.Drawing.Size(202, 17);
-            this.loading_bar.TabIndex = 11;
             // 
             // program_button
             // 
-            this.program_button.Location = new System.Drawing.Point(139, 36);
+            this.program_button.Location = new System.Drawing.Point(381, 39);
             this.program_button.Name = "program_button";
-            this.program_button.Size = new System.Drawing.Size(75, 23);
+            this.program_button.Size = new System.Drawing.Size(75, 64);
             this.program_button.TabIndex = 10;
             this.program_button.Text = "Program";
             this.program_button.UseVisualStyleBackColor = true;
@@ -73,35 +66,55 @@
             this.file_path_text_box.Size = new System.Drawing.Size(363, 20);
             this.file_path_text_box.TabIndex = 8;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 62);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "COM Port";
-            // 
             // com_port_box
             // 
             this.com_port_box.FormattingEnabled = true;
             this.com_port_box.Location = new System.Drawing.Point(12, 38);
             this.com_port_box.Name = "com_port_box";
-            this.com_port_box.Size = new System.Drawing.Size(121, 21);
+            this.com_port_box.Size = new System.Drawing.Size(75, 21);
             this.com_port_box.TabIndex = 6;
             this.com_port_box.DropDown += new System.EventHandler(this.com_port_box_DropDown);
             this.com_port_box.TextChanged += new System.EventHandler(this.com_port_box_TextChanged);
+            // 
+            // loading_bar
+            // 
+            this.loading_bar.BackColor = System.Drawing.Color.Lime;
+            this.loading_bar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.loading_bar.Location = new System.Drawing.Point(53, 51);
+            this.loading_bar.Name = "loading_bar";
+            this.loading_bar.Size = new System.Drawing.Size(353, 27);
+            this.loading_bar.TabIndex = 11;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // loading_label
+            // 
+            this.loading_label.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.loading_label.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.loading_label.Font = new System.Drawing.Font("Corbel Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loading_label.ForeColor = System.Drawing.Color.Lime;
+            this.loading_label.Location = new System.Drawing.Point(144, 25);
+            this.loading_label.Multiline = true;
+            this.loading_label.Name = "loading_label";
+            this.loading_label.Size = new System.Drawing.Size(175, 20);
+            this.loading_label.TabIndex = 0;
+            this.loading_label.Text = "DOWNLOAD";
+            this.loading_label.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(472, 141);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(472, 118);
+            this.Controls.Add(this.loading_label);
             this.Controls.Add(this.loading_bar);
             this.Controls.Add(this.program_button);
             this.Controls.Add(this.browse_button);
             this.Controls.Add(this.file_path_text_box);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.com_port_box);
             this.Name = "main";
             this.Text = "BlackOS Programmer";
@@ -111,14 +124,14 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel loading_bar;
         private System.Windows.Forms.Button program_button;
         private System.Windows.Forms.Button browse_button;
         private System.Windows.Forms.TextBox file_path_text_box;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox com_port_box;
         private System.IO.Ports.SerialPort serial_port;
+        private System.Windows.Forms.Panel loading_bar;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox loading_label;
     }
 }
 
--- BlackOS Programmer/main.cs
+++ BlackOS Programmer/main.cs
@@ -8,16 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.IO;
+using System.Diagnostics;
 
 namespace BlackOS_Programmer
 {
     public partial class main : Form
     {
+        int x, y, w, h;
+
         string com_port;
+
+        int total_bytes;
+        int current_bytes;
+
+        byte[] program;
 
         public main()
         {
             InitializeComponent();
+
+            // Set variables for drawing loading bar border
+            x = loading_bar.Left;
+            y = loading_bar.Top;
+            w = loading_bar.Width;
+            h = loading_bar.Height;
+
+            loading_bar.Width = 0;
+            loading_label.Visible = false;
+
+            file_path_text_box.Text = Properties.Settings.Default.file_name;
         }
 
         private void browse_button_Click(object sender, EventArgs e)
@@ -27,6 +47,8 @@ namespace BlackOS_Programmer
             if (file.ShowDialog() == DialogResult.OK)
             {
                 file_path_text_box.Text = file.FileName;
+                Properties.Settings.Default.file_name = file.FileName;
+                Properties.Settings.Default.Save();
             }
         }
 
@@ -72,10 +94,130 @@ namespace BlackOS_Programmer
         private void program_button_Click(object sender, EventArgs e)
         {
             open_connection();
+            show_loading_screen();
 
+            if (serial_port.IsOpen)
+            {
+                program = File.ReadAllBytes(Properties.Settings.Default.file_name);
+                
+                
 
+                total_bytes = program.Length;
+
+                serial_port.Write("P");
+
+                byte[] bytes = BitConverter.GetBytes(total_bytes);
+                if (BitConverter.IsLittleEndian == false)
+                    Array.Reverse(bytes);
+
+                for (int i = 0; i< 4; i++)
+                {
+                    serial_port.Write(bytes, i, 1);
+                    Wait(10000);
+                }
+
+                this.Update();
+
+                for (int i = 0; i < 4; i++)
+                {
+                    serial_port.Write(program, i, 1);
+                    Wait(10000);
+                }
+
+                for (int i = 0; i < total_bytes; i++)
+                {
+                    serial_port.Write(program, i, 1);
+                    Wait(1000);
+                    current_bytes++;
+                    update_loading();
+                }
+
+                serial_port.Write("P");
+
+                loading_label.Text = "SUCCESS";
+                this.Update();
+
+                timer.Start();
+            }
 
             close_connection();
+        }
+
+        const long microsecondsPerSecond = 1000000;
+
+        static void Wait(long waitMicroseconds)
+        {
+            long waitTicks = (waitMicroseconds * Stopwatch.Frequency) / microsecondsPerSecond;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (sw.ElapsedTicks <= waitTicks) ;
+        }
+
+        private void update_loading()
+        {
+            int new_w = Convert.ToInt32(current_bytes * w / total_bytes);
+
+            loading_bar.Width = new_w;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            hide_loading_screen();
+        }
+
+        private void show_loading_screen()
+        {
+            com_port_box.Visible = false;
+            file_path_text_box.Visible = false;
+            browse_button.Visible = false;
+            program_button.Visible = false;
+
+            loading_label.Visible = true;
+            loading_bar.Visible = true;
+            loading_label.Text = "DOWNLOADING...";
+            loading_label.ForeColor = Color.Lime;
+
+            loading_bar.Width = 0;
+            total_bytes = 0;
+            current_bytes = 0;
+            loading_bar.BackColor = Color.Lime;
+
+            Graphics g = this.CreateGraphics();
+            Pen selPen = new Pen(Color.Lime);
+            g.DrawRectangle(selPen, x - 2, y - 2, w + 3, h + 3);
+            g.Dispose();
+        }
+
+        private void hide_loading_screen()
+        {
+            loading_label.Visible = false;
+            loading_bar.Width = 0;
+
+            Graphics g = this.CreateGraphics();
+            Pen selPen = new Pen(Color.FromArgb(20, 20, 20));
+            g.DrawRectangle(selPen, x - 2, y - 2, w + 3, h + 3);
+            g.Dispose();
+
+            com_port_box.Visible = true;
+            file_path_text_box.Visible = true;
+            browse_button.Visible = true;
+            program_button.Visible = true;
+        }
+
+        private void loading_error()
+        {
+            loading_label.Visible = true;
+            loading_label.Text = "ERROR";
+            loading_label.ForeColor = Color.Red;
+
+            loading_bar.BackColor = Color.Red;
+
+            Graphics g = this.CreateGraphics();
+            Pen selPen = new Pen(Color.Red);
+            g.DrawRectangle(selPen, x - 2, y - 2, w + 3, h + 3);
+            g.Dispose();
         }
     }
 }
